9d5a9592fe42bc13d6224276c63a7c4c05ff998e
Alejandro Fernandez <alejandrof@therapieclinic.com>
2023-05-25 02:05:42 +0100

    Navbar mobile fixed

--- src/components/layout/default-layout/nav-bar/NavBar.tsx
+++ src/components/layout/default-layout/nav-bar/NavBar.tsx
@@ -3,20 +3,25 @@ import { Disclosure } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import { cx } from '@/utils/dom';
 import { useRouter } from '@/hooks/useRouter';
+import ScrollLock from 'react-scrolllock';
 import { NAV_BAR_ITEMS } from './NavBar.constants';
 
 export const NavBar = () => {
   const { getIsCurrentPage } = useRouter();
   return (
-    <div className="bg-gradient-to-r from-primary-red to-primary-red-dark pb-2 shadow-sm sticky top-0 z-10">
-      <Disclosure as="div" className="bg-white">
-        {({ open }) => (
-          <>
+    <Disclosure
+      as="div"
+      className="bg-gradient-to-r from-primary-red to-primary-red-dark pb-2 shadow-sm sticky top-0 z-10"
+    >
+      {({ open }) => (
+        <>
+          <ScrollLock isActive={!!open} />
+          <div className="bg-white">
             <div className="mx-auto max-w-screen-2xl px-2 sm:px-6 lg:px-8">
               <div className="relative flex h-16 items-center justify-between">
                 <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                   {/* Mobile menu button */}
-                  <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 hover:bg-black/10 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 hover:bg-black/10 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-black/60">
                     <span className="sr-only">Open main menu</span>
                     {open ? (
                       <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
@@ -27,12 +32,17 @@ export const NavBar = () => {
                 </div>
                 <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                   <div className="flex flex-shrink-0 items-center">
-                    <Link to="/" className="text-[1.9rem]">
+                    <Disclosure.Button
+                      as={Link}
+                      to="/"
+                      className="text-[1.9rem]"
+                      disabled={!open}
+                    >
                       <strong className="text-transparent bg-clip-text bg-gradient-to-r from-primary-red to-primary-red-dark">
                         Hacker
                       </strong>
                       <span>News</span>
-                    </Link>
+                    </Disclosure.Button>
                   </div>
                   <div className="hidden sm:ml-8 sm:flex items-center">
                     <div className="flex space-x-4">
@@ -61,34 +71,34 @@ export const NavBar = () => {
                 </div>
               </div>
             </div>
-
-            <Disclosure.Panel className="sm:hidden absolute h-full w-full bg-gray-700">
-              <div className="space-y-1 px-2 pb-3 pt-2">
-                {NAV_BAR_ITEMS.map((item) => {
-                  const isActive = getIsCurrentPage(item.path);
-                  return (
+          </div>
+          <Disclosure.Panel className="sm:hidden absolute h-screen w-full bg-gray-700 overscroll-none mt-2">
+            <div className="flex flex-col px-4 py-4">
+              {NAV_BAR_ITEMS.map((item) => {
+                const isActive = getIsCurrentPage(item.path);
+                return (
+                  <div className="border-b" key={item.title}>
                     <Disclosure.Button
-                      key={item.title}
                       as={Link}
                       to={item.path}
                       className={cx(
                         {
-                          'bg-gray-900 ': isActive,
+                          'bg-black/30': isActive,
                           ' hover:bg-gray-700': !isActive,
                         },
-                        'block rounded-md px-3 py-2 text-base font-medium'
+                        'block px-3 py-3 text-base font-medium text-white'
                       )}
                       aria-current={isActive ? 'page' : undefined}
                     >
                       {item.title}
                     </Disclosure.Button>
-                  );
-                })}
-              </div>
-            </Disclosure.Panel>
-          </>
-        )}
-      </Disclosure>
-    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
   );
 };
