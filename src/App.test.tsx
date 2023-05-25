f3b0cd23481beb1a9b3119952a152b5c588d2d54
Alejandro Fernandez <alejandrof@therapieclinic.com>
2023-05-25 10:32:42 +0100

    Tests fix

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,24 @@
 import { render, screen } from '@testing-library/react';
 import { describe, it } from 'vitest';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
+
+const WrappedApp = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 describe('App', () => {
   it('Renders Main Title', () => {
-    render(<App />);
+    render(<WrappedApp />);
 
     expect(screen.getByRole('heading', { level: 1 })).toBeInTheDocument();
   });
   it('Renders Header and Navbar desktop and mobile', () => {
-    render(<App />);
+    render(<WrappedApp />);
 
     expect(
       screen.getByRole('banner', { name: 'Main header' })
